f3b2b563ee34dc8705c9fe2ab5b6107486fc5c41
Ilya Svyahzin <105287964+svyazhin00@users.noreply.github.com>
2023-02-14 17:46:42 +0300

    Add last task try 3

--- LastTask.py
+++ LastTask.py
@@ -1,19 +1,23 @@
 def func_max(x=None, count=1):
-    i = int(input())
-    if i == 0:
-        return x, count
-    else:
-        if x is None:
-            x = i
-            count = 1
-            return func_max(x, count)
+    try:
+        i = int(input())
+        if i == 0:
+            return x, count
         else:
-            if i < x:
+            if x is None:
+                x = i
+                count = 1
                 return func_max(x, count)
-            elif i > x:
-                return func_max(i, count)
             else:
-                count += 1
-                return func_max(i, count)
+                if i < x:
+                    return func_max(x, count)
+                elif i > x:
+                    return func_max(i, count)
+                else:
+                    count += 1
+                    return func_max(i, count)
+    except:
+
+        raise TypeError('Type must be integer!')
 
 print(func_max())
